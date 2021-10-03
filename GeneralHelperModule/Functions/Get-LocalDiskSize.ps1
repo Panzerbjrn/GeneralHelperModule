55ff49726671af5a013dbe59f1cf17e51829b421
Lars.Petersson <lars@panzerbjrn.eu>
2021-10-03 13:07:09 +0200

    updated function to be forwards compatible

--- GeneralHelperModule/Functions/Get-LocalDiskSize.ps1
+++ GeneralHelperModule/Functions/Get-LocalDiskSize.ps1
@@ -1,9 +1,11 @@
-Function Get-LocalDiskSize
-{
-$TempPath = Test-Path "C:\Temp\"
-IF ($TempPath -eq $False) {New-Item -ItemType "Directory" -Path C:\TEMP -Force}
-ELSE {}
-Write-Host "Getting Disk Sizes"
-Get-WmiObject Win32_LogicalDisk -filter "DriveType=3" -computer $ENV:Computername | Select-Object SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}} | Export-Csv C:\TEMP\DiskSize.CSV -NoTypeInformation -Delimiter "`t"
-notepad C:\temp\DiskSize.CSV
+Function Get-LocalDiskSize{
+	$TempPath = Test-Path "C:\Temp\"
+	IF ($TempPath -eq $False) {New-Item -ItemType "Directory" -Path C:\TEMP -Force}
+	Write-Output "Getting Disk Sizes"
+	Get-CimInstance -ClassName Win32_LogicalDisk -filter "DriveType=3" | Select SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}},@{Name="Free";Expression={"{0:N1}" -f($_.freespace/1gb)}},@{Name="Percent";Expression={[int]($_.FreeSpace/$_.Size * 100)}}
+	#Get-WmiObject Win32_LogicalDisk -filter "DriveType=3" -computer $ENV:Computername | Select-Object SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}}
+
+	#| Export-Csv C:\TEMP\DiskSize.CSV -NoTypeInformation -Delimiter "`t"
+	
+	#notepad C:\temp\DiskSize.CSV
 }
