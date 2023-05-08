69931835b151768584f7c3d52610c3e7536bd531
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2023-05-08 20:08:35 +0200

    Refreshing

--- GeneralHelperModule/Functions/Get-LastReboot.ps1
+++ GeneralHelperModule/Functions/Get-LastReboot.ps1
@@ -1,20 +1,19 @@
 Function Get-LastReboot{
 <#
 	.SYNOPSIS
-		Get-LastReboot is designed to return the last reboot time of a server or client.
+		Get-LastReboot is designed to return the last reboot time of a windows computer.
 	.DESCRIPTION
-		Get-LastReboot filters the system event logs looking for the last reboot event (or shutdown) and returns all the information related to the incident.
+		Get-LastReboot filters the system event logs looking for the last reboot event (or shutdown) and returns all the information related to the reboot.
 	.PARAMETER Computer
-		The name of the computer that you wish to determine it's last boot up time.
-		If left blank, it defaults to the local machine.
+		The name of the computer  you wish to interrogate. Default is your local machine.
 	.EXAMPLE
 		Get-LastReboot DC1
 		
 		This will return the last reboot time of DC1
 	.EXAMPLE
-		Get-ChildItem AD:"OU=Servers,OU=Computers,DC=Contoso,DC=Local" | Select Name | Get-LastReboot
+		Get-ChildItem AD:"OU=Servers,OU=Computers,DC=CentralIndustrial,DC=EU" | Select Name | Get-LastReboot
 		
-		This grabs the last reboots for any machines in the Servers OU.
+		This outputs the last reboot for all machines in the Servers OU.
 	.OUTPUTS
 		Object: System.Management.Automation.PSCustomObject
 		IF it was a clean boot:
@@ -37,43 +36,44 @@ Function Get-LastReboot{
 				LastBoot		[DateTime]
 				Message			[String]
 				UpTime			[TimeSpan]
-	.NOTES
-		Author: Twon of An
-	.LINK
-		New-Timespan
-		Get-EventLog
 #>
 	Param(
-		[Parameter(Position=0,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
 		[alias("Name","ComputerName")]
-		[string[]]$Computer=@($env:ComputerName)
+		[string[]]$Computer = @($env:ComputerName)
 	)
 	Begin{
 		$Result = @()
 	}
 	Process{
-		ForEach($machine in $Computer){
-			If(-not (Test-Connection -ComputerName $machine -Count 1 -quiet)){ #Make sure we can connect to it...
-				Write-Host "$([string]$machine.toupper()) cannot be reached..."
+		ForEach($Machine in $Computer){
+			IF(-not (Test-Connection -ComputerName $Machine -Count 1 -quiet)){ #Make sure we can connect to it...
+				Write-OutPut "$([string]$Machine.toupper()) cannot be reached..."
 				Break
 			}
-			Else{
-				If($machine -ne $env:computername){ #If it is a remote machine, make sure RemoteRegistry is running so we can access the logs
-					$RegServ = Get-Service remoteregistry -ComputerName $machine
-					If($RegServ.status -ne "Running"){
-						Set-Service remoteregistry -ComputerName $machine -status Running
+			ELSE{
+				IF($Machine -ne $env:ComputerName){ #If it is a remote machine, make sure RemoteRegistry is running so we can access the logs
+					$RegServ = Get-Service remoteregistry -ComputerName $Machine
+					IF($RegServ.status -ne "Running"){
+						Set-Service remoteregistry -ComputerName $Machine -status Running
 					}
 				}
-				$SuccessfullReboot = Get-EventLog system -ComputerName $machine -InstanceId 2147484722 -Newest 1
-				$UnSuccessfullReboot = Get-EventLog system -ComputerName $machine -InstanceId 41 -Newest 1
-				$Event = If($SuccessfullReboot.TimeWritten -gt $UnSuccessfullReboot.Timewritten){$SuccessfullReboot;$Cleanboot = $true}Else{$UnSuccessfullReboot}
+				IF ($PSVersionTable.PSVersion.Major -eq 5){
+					$SuccessfullReboot = Get-EventLog system -ComputerName $Machine -InstanceId 2147484722 -Newest 1
+					$UnSuccessfullReboot = Get-EventLog system -ComputerName $Machine -InstanceId 41 -Newest 1
+				}
+				IF ($PSVersionTable.PSVersion.Major -eq 7){
+					$SuccessfullReboot = Get-WinEvent -FilterHashtable @{"Id"=1074;"Logname"="System"} -ComputerName $Machine -MaxEvents 1
+					$UnSuccessfullReboot = Get-WinEvent -FilterHashtable @{"Id"=41;"Logname"="System"} -ComputerName $Machine -MaxEvents 1
+				}
+				$Event = IF($SuccessfullReboot.TimeWritten -gt $UnSuccessfullReboot.Timewritten){$SuccessfullReboot;$Cleanboot = $True}ELSE{$UnSuccessfullReboot}
 				$LastRebootTime = $Event.TimeGenerated
-				$UpTime = New-TimeSpan -start $LastRebootTime -end $(Get-Date)
-				If($CleanBoot){
+				$UpTime = New-TimeSpan -start $LastReboot T ime -end $(Get-Date)
+				IF($CleanBoot){
 					$Result = $Result + (New-Object PSObject -Property @{ #Build the object for return
 						"Code" = $Event.ReplacementStrings[3]
 						"Comment" = $Event.ReplacementStrings[5]
-						"ComputerName" = $machine.toupper()
+						"ComputerName" = $Machine.toupper()
 						"LastBoot" = $LastRebootTime
 						"Message" = $Event.Message
 						"Process" = $Event.ReplacementStrings[0]
@@ -81,12 +81,12 @@ Function Get-LastReboot{
 						"Type" = $Event.ReplacementStrings[4]
 						"UpTime" = $UpTime
 						"User" = $Event.ReplacementStrings[6]
-						"CleanBoot" = $true
+						"CleanBoot"  =  $True
 					})
 				}
-				Else{
+				ELSE{
 					$Result = $Result + (New-Object PSObject -Property @{ #Build the object for return
-						"ComputerName" = $machine.toupper()
+						"ComputerName" = $Machine.toupper()
 						"LastBoot" = $LastRebootTime
 						"Message" = $Event.Message
 						"UpTime" = $UpTime
