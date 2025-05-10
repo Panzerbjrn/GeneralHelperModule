7fff3eb059daec414dd4b293b10c3e8bbabe0fcb
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2025-05-10 12:12:20 +0200

    Refreshing

---
+++ GeneralHelperModule/Functions/Test-ChocoUpdates.ps1
@@ -0,0 +1,12 @@
+Function Test-ChocoUpdates {
+    param([string[]]$Apps)
+    foreach ($App in $Apps) {
+        $Status = choco outdated --ignore-pinned --limit-output | Where-Object { $_ -like "$App|*" }
+        if ($Status) {
+            $Current, $Available, $Pinned = $Status.Split('|')[1,2,3]
+            Write-Host "$App : Update available ($Current → $Available)" -ForegroundColor Yellow
+        } else {
+            Write-Host "$App : Up to date" -ForegroundColor Green
+        }
+    }
+}
